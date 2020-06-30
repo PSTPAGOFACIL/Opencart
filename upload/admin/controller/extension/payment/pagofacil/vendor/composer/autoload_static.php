bd4ae6fc8c79b751e456c01e5f04814ee3be97e7
oscar Aguilera <oscar@aguilera.xyz>
2020-06-30 18:16:18 -0400

    modified:   README.md 	modified:   upload/admin/controller/extension/payment/pagofacil.php 	new file:   upload/admin/controller/extension/payment/pagofacil/composer.json 	new file:   upload/admin/controller/extension/payment/pagofacil/composer.lock 	renamed:    upload/catalog/controller/extension/payment/pagofacil/vendor/autoload.php -> upload/admin/controller/extension/payment/pagofacil/vendor/autoload.php 	renamed:    upload/catalog/controller/extension/payment/pagofacil/vendor/composer/ClassLoader.php -> upload/admin/controller/extension/payment/pagofacil/vendor/composer/ClassLoader.php 	renamed:    upload/catalog/controller/extension/payment/pagofacil/vendor/composer/LICENSE -> upload/admin/controller/extension/payment/pagofacil/vendor/composer/LICENSE 	renamed:    upload/catalog/controller/extension/payment/pagofacil/vendor/composer/autoload_classmap.php -> upload/admin/controller/extension/payment/pagofacil/vendor/composer/autoload_classmap.php 	renamed:    upload/catalog/controller/extension/payment/pagofacil/vendor/composer/autoload_namespaces.php -> upload/admin/controller/extension/payment/pagofacil/vendor/composer/autoload_namespaces.php 	renamed:    upload/catalog/controller/extension/payment/pagofacil/vendor/composer/autoload_psr4.php -> upload/admin/controller/extension/payment/pagofacil/vendor/composer/autoload_psr4.php 	renamed:    upload/catalog/controller/extension/payment/pagofacil/vendor/composer/autoload_real.php -> upload/admin/controller/extension/payment/pagofacil/vendor/composer/autoload_real.php 	renamed:    upload/catalog/controller/extension/payment/pagofacil/vendor/composer/autoload_static.php -> upload/admin/controller/extension/payment/pagofacil/vendor/composer/autoload_static.php 	new file:   upload/admin/controller/extension/payment/pagofacil/vendor/composer/installed.json 	new file:   upload/admin/controller/extension/payment/pagofacil/vendor/kdu/pagofacilcore 	modified:   upload/admin/language/en-gb/extension/payment/pagofacil.php 	modified:   upload/admin/language/es-cl/extension/payment/pagofacil.php 	modified:   upload/admin/language/es-es/extension/payment/pagofacil.php 	new file:   upload/admin/model/extension/payment/pagofacil.php 	modified:   upload/admin/view/image/payment/logo_pagofacil.png 	modified:   upload/admin/view/template/extension/payment/pagofacil.twig 	modified:   upload/catalog/controller/extension/payment/pagofacil.php 	deleted:    upload/catalog/controller/extension/payment/pagofacil/composer.json 	deleted:    upload/catalog/controller/extension/payment/pagofacil/composer.lock 	deleted:    upload/catalog/controller/extension/payment/pagofacil/vendor/composer/installed.json 	deleted:    upload/catalog/controller/extension/payment/pagofacil/vendor/pagofacil/php-sdk/.gitignore 	deleted:    upload/catalog/controller/extension/payment/pagofacil/vendor/pagofacil/php-sdk/README.md 	deleted:    upload/catalog/controller/extension/payment/pagofacil/vendor/pagofacil/php-sdk/classes/Operacion.php 	deleted:    upload/catalog/controller/extension/payment/pagofacil/vendor/pagofacil/php-sdk/classes/Request.php 	deleted:    upload/catalog/controller/extension/payment/pagofacil/vendor/pagofacil/php-sdk/classes/Response.php 	deleted:    upload/catalog/controller/extension/payment/pagofacil/vendor/pagofacil/php-sdk/classes/Transaction.php 	deleted:    upload/catalog/controller/extension/payment/pagofacil/vendor/pagofacil/php-sdk/composer.json 	modified:   upload/catalog/language/en-gb/extension/payment/pagofacil.php 	modified:   upload/catalog/language/es-cl/extension/payment/pagofacil.php 	modified:   upload/catalog/language/es-es/extension/payment/pagofacil.php 	modified:   upload/catalog/model/extension/payment/pagofacil.php 	modified:   upload/catalog/view/theme/default/image/logo_pagofacil.png
    	modified:   upload/admin/controller/extension/payment/pagofacil/vendor/kdu/pagofacilcore (modified content)

--- upload/admin/controller/extension/payment/pagofacil/vendor/composer/autoload_static.php
+++ upload/admin/controller/extension/payment/pagofacil/vendor/composer/autoload_static.php
@@ -4,27 +4,27 @@
 
 namespace Composer\Autoload;
 
-class ComposerStaticInit40e4bbaed45e3dae7c27e9248ee5a0ff
+class ComposerStaticInitde901528f4017e3d8ac8914fac5a9cca
 {
     public static $prefixLengthsPsr4 = array (
         'P' => 
         array (
-            'PagoFacil\\lib\\' => 14,
+            'PagoFacilCore\\' => 14,
         ),
     );
 
     public static $prefixDirsPsr4 = array (
-        'PagoFacil\\lib\\' => 
+        'PagoFacilCore\\' => 
         array (
-            0 => __DIR__ . '/..' . '/pagofacil/php-sdk/classes',
+            0 => __DIR__ . '/..' . '/kdu/pagofacilcore/src',
         ),
     );
 
     public static function getInitializer(ClassLoader $loader)
     {
         return \Closure::bind(function () use ($loader) {
-            $loader->prefixLengthsPsr4 = ComposerStaticInit40e4bbaed45e3dae7c27e9248ee5a0ff::$prefixLengthsPsr4;
-            $loader->prefixDirsPsr4 = ComposerStaticInit40e4bbaed45e3dae7c27e9248ee5a0ff::$prefixDirsPsr4;
+            $loader->prefixLengthsPsr4 = ComposerStaticInitde901528f4017e3d8ac8914fac5a9cca::$prefixLengthsPsr4;
+            $loader->prefixDirsPsr4 = ComposerStaticInitde901528f4017e3d8ac8914fac5a9cca::$prefixDirsPsr4;
 
         }, null, ClassLoader::class);
     }
